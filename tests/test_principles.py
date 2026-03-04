a8b339dd266245a4104bb617a3cb7fba6296404a
Herman Geets <dogfish520@gmail.com>
2026-03-04 11:13:52 +0300

    Show test-not-grant-you-from-error principle

--- tests/test_principles.py
+++ tests/test_principles.py
@@ -3,11 +3,22 @@ sys.path.append("../src")
 
 #TODO make it with `pip install -e .`
 
-from math_demo import add
+from math_demo import (
+    add,
+    add_with_bug
+)
 
-def test_principles():
-    assert add(2, 2) == 4
-    print("Test BASIC ADDITIONS")
+def test_addition():
+    assert add(2, 2) == 4, "Function should return 4"
+    print("Test BASIC ADDITIONS PASSED")
+
+def test_addition_with_bug():
+    assert add_with_bug(2, 2) == 4, "Function should return 4"
+    assert add_with_bug(0, 0) == 0
+    print("Test BASIC ADDITIONS WITH BUG PASSED")
+    # assert add_with_bug(6, 7) == 13 # Ты дурак?
+
 
 if __name__ == "__main__":
-    test_principles()
+    test_addition()
+    test_addition_with_bug()
