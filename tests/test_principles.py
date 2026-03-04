91ed8057694eb536302048b2739c7cd5304187fb
Herman Geets <dogfish520@gmail.com>
2026-03-04 11:41:15 +0300

    Added test for reasonable number of cases and logically separated

--- tests/test_principles.py
+++ tests/test_principles.py
@@ -31,9 +31,27 @@ def test_addition_overcomplicated():
             assert add(i, -j) == sum([i, -j])
             assert add(-i, -j) == sum([-i, -j])
 
+def test_addition_reasonable():
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(6, 7) == 13
+    assert add(-6, -7) == -13
+    assert add(6, -7) == -1
+    assert add(-7, 0) == -7
+    assert add(7, 0) == 7
+    print("Test BASIC REASONABLE PASSED")
+
+def test_addition_commutative():
+    # can be previous test but logically separated
+    assert add(7, -6) == 1
+    assert add(-6, 7) == 1
+    print("Test BASIC COMMUTATIVE PASSED")
+
 
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_duplicated()
     # test_addition_overcomplicated()
+    test_addition_reasonable()
+    test_addition_commutative()
