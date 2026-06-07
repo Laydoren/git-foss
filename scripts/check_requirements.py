a92937e8bef7928bc75d30c11e4dbc2ab9ddc661
Lay <dogfish520@gmail.com>
2026-06-07 16:04:10 +0800

    Fix: fix check_requirements

--- scripts/check_requirements.py
+++ scripts/check_requirements.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-result = subprocess.run([sys.executable, "-m", "pip_missing_reqs", "src"], capture_output=True, text=True)
+result = subprocess.run([sys.executable, "-m", "pip_check_reqs.find_missing_reqs", "src"], capture_output=True, text=True)
 
 if result.stdout:
     print(result.stdout)
@@ -12,4 +12,4 @@ if result.returncode != 0:
     print("Missing dependencies found!")
     raise SystemExit(1)
 
-print("Requirements check passed")
+print("Requirements check passed.")
